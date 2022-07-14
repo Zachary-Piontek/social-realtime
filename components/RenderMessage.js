4fefcb9dafdd018e265ac422669666b4a45b79bc
Aphenphos <MaxwillWinters@gmail.com>
2022-07-14 15:57:43 -0400

    live update works

--- components/RenderMessage.js
+++ components/RenderMessage.js
@@ -1,25 +1,23 @@
 function Message({ message }) {
+    console.log(message);
     const li = document.createElement('li');
     li.classList.add('message-container');
 
     const p = document.createElement('p');
     p.classList.add('message-content');
+    console.log(message.content);
     p.textContent = message.content;
 
-    const username = document.createElement('h3');
-    username.textContent = message.username;
-
-    li.append(p, username);
+    li.append(p);
 }
 
 export function renderMessages(root) {
     return ({ messages }) => {
+        console.log(messages);
         root.innerHTML = '';
 
         for (const message of messages) {
-            const item = Message({
-                message
-            });
+            const item = Message({ message });
             root.append(item);
         }
     };
